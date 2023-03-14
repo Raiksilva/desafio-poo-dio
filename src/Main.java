4fc5cb165246f893dea6a394e981c50f6851368f
raiksilva <raiksilva2016@gmail.com>
2023-03-14 20:39:59 -0300

    Mentoria e curso

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Java curso");
+        curso1.setDescricao("Aprendendo a programar com paradigma POO");
+        curso1.setCargaHoraria(10);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso C#");
+        curso2.setDescricao("Aprendendo a programar com paradigma POO em C#");
+        curso2.setCargaHoraria(7);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Desenvolvendo em java de forma rápida");
+        mentoria1.setDescricao("Depois desse curso, você conseguirar entrar ");
+        mentoria1.setData(LocalDate.now());
+
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria1);
     }
 }
